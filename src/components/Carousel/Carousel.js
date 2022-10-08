c5d227a6a088408369b0076fe9e451ef77751e5f
goranaviani <goranaviani@gmail.com>
2022-10-08 15:00:10 +0200

    index page

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -8,6 +8,11 @@ import "swiper/css/navigation";
 // import required modules
 import { Autoplay, Pagination, Navigation } from "swiper";
 import {image1, image2} from "../../static/images/images";
+import Typography from '@mui/material/Typography';
+
+const TEXT = {
+    handSewnOrganicWool: "Hand sewn organic wool"
+}
 
 const Carousel = () => {
     console.log({image1})
@@ -32,22 +37,15 @@ const Carousel = () => {
         className="mySwiper"
       >
         <SwiperSlide>
-         <div className="CarouselCard"></div>
             <img src={image2} />
           <div className="text" data-swiper-parallax="-100">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-              dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
-              laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
-              Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
-              Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
-              ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
-              tincidunt ut libero. Aenean feugiat non eros quis feugiat.
-            </p>
+            <Typography sx={{fontWeight: 'bold'}} variant="h4"
+                                        component="div">
+                                {TEXT.handSewnOrganicWool}
+                            </Typography>
           </div>
 </SwiperSlide>
         <SwiperSlide>
-            <div className="CarouselCard"></div>
             <img src={image2} />
           <div className="text" data-swiper-parallax="-100">
             <p>
