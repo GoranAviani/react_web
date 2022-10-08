f47c7891b4242c983ba682d5ffc5593e35382597
goranaviani <goranaviani@gmail.com>
2022-10-08 15:52:14 +0200

    index page

---
+++ src/components/QuickFacts/QuickFacts.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import 'QuickFacts.css'
+
+const QuickFacts = () => {
+
+    return(
+        <div>
+            facts
+        </div>
+    )
+}
+
+export default QuickFacts
