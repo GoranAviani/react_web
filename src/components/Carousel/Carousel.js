d4254d14ce2bbc3546243784bb2e3f8b23029910
goranaviani <goranaviani@gmail.com>
2022-10-08 14:27:54 +0200

    index page

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -32,7 +32,38 @@ const Carousel = () => {
         modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper"
       >
-        <SwiperSlide>          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+        <SwiperSlide>
+            <div
+          slot="container-start"
+          className="parallax-bg"
+          style={{
+            "background-image":
+              "url(https://swiperjs.com/demos/images/nature-1.jpg)",
+              "width": "100%",
+              "height": "100%",
+          }}
+          data-swiper-parallax="-23%"
+        >
+
+
+             <div className="title" data-swiper-parallax="-300">
+            Slide 1
+          </div>
+          <div className="subtitle" data-swiper-parallax="-200">
+            Subtitle
+          </div>
+          <div className="text" data-swiper-parallax="-100">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+              dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
+              laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
+              Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
+              Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
+              ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
+              tincidunt ut libero. Aenean feugiat non eros quis feugiat.
+            </p>
+          </div>
+                </div>
 </SwiperSlide>
         <SwiperSlide>          <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
 </SwiperSlide>
