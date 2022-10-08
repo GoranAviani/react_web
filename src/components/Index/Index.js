51e042e7990802da2c70e4b1fc71cfcd382b2642
goranaviani <goranaviani@gmail.com>
2022-10-08 09:20:11 +0200

    index page

--- src/components/Index/Index.js
+++ src/components/Index/Index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Index.css'
 
+import Navigation from "../Navigation/Navigation";
 const Index = () => {
 
     return(
         <div>
+            <Navigation/>
             this is index
         </div>
 
