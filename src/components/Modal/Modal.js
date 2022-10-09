64d77bd0a6aabf62ab1bf2f0c67cb3d169af7174
goranaviani <goranaviani@gmail.com>
2022-10-09 18:26:46 +0200

    building new pages

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -20,23 +20,29 @@ const BasicModal = ({open, handleClose}) => {
 
 
     return (
-        <div>
-            <Modal
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
-            >
-                <Box sx={style}>
-                    <Typography id="modal-modal-title" variant="h6" component="h2">
-                        Text in a modal
-                    </Typography>
-                    <Typography id="modal-modal-description" sx={{mt: 2}}>
-                        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-                    </Typography>
-                </Box>
-            </Modal>
-        </div>
+        <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+        >
+            <Box sx={style}>
+                <Typography id="modal-modal-title" variant="h6" component="h2">
+                    Text in a modal
+                </Typography>
+                <Typography id="modal-modal-description" sx={{mt: 2}}>
+                    Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+                </Typography>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={() => {
+                        handleClose()
+                    }}
+                >Close
+                </Button>
+            </Box>
+        </Modal>
     );
 }
 
