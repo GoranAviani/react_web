1c91520bd6db0c1cd1391e1ffce4e1e1f4920730
goranaviani <goranaviani@gmail.com>
2022-10-09 18:24:22 +0200

    building new pages

--- src/components/QuickFacts/partials/QuickFactsInfoRightSide.js
+++ src/components/QuickFacts/partials/QuickFactsInfoRightSide.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
+import BasicModal from "../../Modal/Modal";
+import OurWool from "../ExtraPages/OurWool";
+import Button from "@mui/material/Button";
 
 const QuickFactsInfoRightSide = ({text, linkText, linkUrl}) => {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
 
     return (
         <div className="quickFactsInfoRightSide">
@@ -12,10 +18,15 @@ const QuickFactsInfoRightSide = ({text, linkText, linkUrl}) => {
             </Typography>
 
             <Link color="#000"
-                  href={linkUrl}
+                  onClick={() => {
+                      {
+                          handleOpen()
+                      }
+                  }}
             >
                 {linkText}
             </Link>
+            <BasicModal open={open} handleClose={handleClose}/>
         </div>
 
     )
