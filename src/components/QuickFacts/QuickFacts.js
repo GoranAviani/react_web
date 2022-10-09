a29a929dab682abd3315dde768822cae1b2bc2a4
goranaviani <goranaviani@gmail.com>
2022-10-09 14:21:01 +0200

    Building index page

--- src/components/QuickFacts/QuickFacts.js
+++ src/components/QuickFacts/QuickFacts.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import './QuickFacts.css'
-import {sheep_color_64, knitting_color_64, shipped_color_64, planet_earth_color_64} from "../../static/icons/icons";
+import {sheep_64, wool_64, shipped_64, planet_earth_64} from "../../static/icons/icons";
 
 const QuickFacts = () => {
 
     return (
         <div className="quickFacts">
             <div className="quickFactsInfo">
-                <img src={sheep_color_64}/>
+                <img src={sheep_64}/>
             </div>
             <div>
-                <img src={knitting_color_64}/>
+                <img src={wool_64}/>
 
             </div>
             <div>
-                <img src={shipped_color_64}/>
+                <img src={shipped_64}/>
             </div>
             <div>
                 <img src={planet_earth_64}/>
