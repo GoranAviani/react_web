f6426c2a57598ce39a352ac803e655cc802620ee
goranaviani <goranaviani@gmail.com>
2022-10-09 10:28:56 +0200

    Building index page

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,14 +6,7 @@ const Footer = () => {
     return (
         <div>
             this is footer
-            <a href="https://www.flaticon.com/free-icons/sheep" title="sheep icons">Sheep icons created by Freepik -
-                Flaticon</a>
-            <a href="https://www.flaticon.com/free-icons/wool" title="wool icons">Wool icons created by Freepik -
-                Flaticon</a>
-            <a href="https://www.flaticon.com/free-icons/delivery" title="delivery icons">Delivery icons created by
-                Freepik - Flaticon</a>
-            <a href="https://www.flaticon.com/free-icons/environment" title="environment icons">Environment icons
-                created by Freepik - Flaticon</a>
+
         </div>
     )
 }
