02c42b464f05417962750b869b15108d37dfe537
goranaviani <goranaviani@gmail.com>
2022-10-20 18:22:16 +0200

    footer changes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './Footer.css'
 import Typography from "@mui/material/Typography";
 import Links from "./partials/Links";
 import {aboutCompanyFooterLinks, yourBenefitsFooterLinks, helpFooterLinks} from "../../Route_urls";
 import {facebook_16} from "../../static/icons/icons";
 import Spacer from "../Spacer";
+import SendIcon from '@mui/icons-material/Send';
+import Button from '@mui/material/Button';
+import useWindowDimensions from "../hooks/WindowDimension/useWindowDimensions";
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
 
 const TEXT = {
     aboutWool: "About wool",
@@ -12,32 +17,66 @@ const TEXT = {
     help: "Help",
 }
 const Footer = () => {
+    const [showAboutWoolMobileHeader, setShowAboutWoolMobileHeader] = useState(false)
+    const [showBenefitsMobileHeader, setShowBenefitsMobileHeader] = useState(false)
+    const [showHelpMobileHeader, setShowHelpMobileHeader] = useState(false)
+    const {isMobile} = useWindowDimensions()
+    console.log(isMobile)
+    useEffect(() => {
+        if (!isMobile) {
+            setShowAboutWoolMobileHeader(true)
+        }
+    })
+
     return (
         <div className="Footer">
             <div className='SiteLinks'>
                 <div>
                     <div className='Header'>
-                        <Typography sx={{fontWeight: 700}}>
+                        {isMobile ? <Button onClick={() => {
+                            setShowAboutWoolMobileHeader(!showAboutWoolMobileHeader)
+                        }} endIcon={showAboutWoolMobileHeader ? <RemoveIcon/> : <AddIcon/>}>
+                            {TEXT.aboutWool}
+                        </Button> : <Typography sx={{fontWeight: 700}}>
                             {TEXT.aboutWool}
                         </Typography>
+                        }
                     </div>
-                    <Links linksArray={aboutCompanyFooterLinks}/>
+                    {isMobile && showAboutWoolMobileHeader &&
+                        <Links linksArray={aboutCompanyFooterLinks}/>}
                 </div>
                 <div>
                     <div className='Header'>
-                        <Typography sx={{fontWeight: 700}}>
-                            Benefits
+                        {isMobile ? <Button onClick={() => {
+                            setShowBenefitsMobileHeader(!showBenefitsMobileHeader)
+                        }} endIcon={<SendIcon/>}>
+                            {TEXT.benefits}
+                        </Button> : <Typography sx={{fontWeight: 700}}>
+                            {TEXT.benefits}
                         </Typography>
+                        }
                     </div>
-                    <Links linksArray={yourBenefitsFooterLinks}/>
+                    {isMobile && showBenefitsMobileHeader &&
+                        <Links linksArray={yourBenefitsFooterLinks}/>}
+
+
                 </div>
                 <div>
+
                     <div className='Header'>
-                        <Typography sx={{fontWeight: 700}}>
-                            Help
+                        {isMobile ? <Button onClick={() => {
+                            setShowHelpMobileHeader(!showHelpMobileHeader)
+                        }} endIcon={<SendIcon/>}>
+                            {TEXT.help}
+                        </Button> : <Typography sx={{fontWeight: 700}}>
+                            {TEXT.help}
                         </Typography>
+                        }
                     </div>
-                    <Links linksArray={helpFooterLinks}/>
+                    {isMobile && showHelpMobileHeader &&
+                        <Links linksArray={helpFooterLinks}/>}
+
+
                 </div>
             </div>
             <div className='PaymentMethods'>
