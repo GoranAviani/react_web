57f1d80701982448cf6ceb637a0e34e44b82d998
goranaviani <goranaviani@gmail.com>
2022-10-20 20:46:37 +0200

    footer changes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -64,8 +64,7 @@ const Footer = () => {
                                         }}>
                                     {TEXT.benefits}
                                 </Button>
-                                <Button disableRipple style={{minWidth: "10%",                 color: "#000",
-}}
+                                <Button disableRipple style={{minWidth: "10%", color: "#000",}}
                                         onClick={() => {
                                             setShowBenefitsMobileHeader(!showBenefitsMobileHeader)
                                         }} endIcon={showBenefitsMobileHeader ? <RemoveIcon/> : <AddIcon/>}>
