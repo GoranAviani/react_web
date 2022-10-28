e93946810e331281af8c0f0328bd5f3316271ee0
goranaviani <goranaviani@gmail.com>
2022-10-28 17:00:17 +0200

    product changes

---
+++ src/components/ProductList/partials/ProductCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {klarnaCheckout} from "../TEST_CHECKOUT";
+
+const ProductCard = () => {
+
+
+    return (
+        <>
+            {klarnaCheckout.html_snippet}
+
+        </>
+    )
+}
+
+export default ProductCard;
